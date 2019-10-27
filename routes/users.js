8407eb726803cbc6c65c55d15a210388700f30e2
Hamid raza <hamidraza.chemistry@gmail.com>
2019-10-27 06:58:05 -0700

    fourth commit

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ var api = express.Router();
 
 var data = [];
 
-api.get('/hello',function (req,res) {
+api.get('/get',function (req,res) {
     res.send({data: data})
     console.log(data);
 });
 
-api.post('/hello',function (req,res) {
+api.post('/post',function (req,res) {
     data.push(req.body);
     console.log(req.body);
     res.send({status: 200, data: data})
