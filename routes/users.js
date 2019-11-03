2a00bed0139027dd6f7bb4f98a465d578bea087d
Hamid raza <hamidraza.chemistry@gmail.com>
2019-11-03 02:25:55 -0800

    update

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,37 @@
 var express = require("express");
 var api = express.Router();
+var schema = require('./schema');
+var UserModel = schema.UserModel;
 
-var data = [];
 
-api.get('/get',function (req,res) {
+var o = {
+    name: "Hello User",
+    email: "asadas@gmail.com",
+};
+
+
+setTimeout(function () {
+    UserModel.find().exec(function (error, data) {
+        console.log(error);
+        console.log(data);
+    })
+
+
+    console.log(88);
+   /* var user = new UserModel(o);
+    user.save().then(function (error, data) {
+        console.log(error);
+        console.log(data);
+    });*/
+}, 5000)
+
+
+api.get('/get', function (req, res) {
     res.send({data: data})
     console.log(data);
 });
 
-api.post('/post',function (req,res) {
+api.post('/post', function (req, res) {
     data.push(req.body);
     console.log(req.body);
     res.send({status: 200, data: data})
--- server5.js
+++ server5.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require("path");
 var cors = require('cors');
+var mongoose = require("mongoose");
+
+
+// var uri = "mongodb+srv://admin:admin@hamid-yq7y8.mongodb.net/test?retryWrites=true&w=majority";
+var uri = "mongodb://elms:elms@ds025419.mlab.com:25419/elms";
+mongoose.connect(uri , {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('connection',function () {
+    console.log("Connected to mongodb");
+});
+
+
+
 var users = require('./routes/users');
 var app = express();
 
-var data = [];
 
 app.use(bodyParser.json({limit: '5000kb'}));
 app.use(bodyParser.urlencoded({extended: false}));
